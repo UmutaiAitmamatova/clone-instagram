48fccd40b31f76264c61ce836d4606a460eb3125
Umutai <umutaiaitmamativa@gmail.com>
2023-03-15 23:58:24 +0600

    start signup page

---
+++ src/pages/signUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './signUp.scss'
+
+function SignUp() {
+    console.log('hello');
+  return (
+    <div className='sign_up'>
+        SignUp
+        
+    </div>
+  )
+}
+
+export default SignUp;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from '../pages/signIn';
+import SignUp from '../pages/signUp';
 
 function Router() {
   return (
     <>
     <Routes>
         <Route path="/signIn" element={<SignIn/>} />
+        <Route path="/signUn" element={<SignUp/>} />
     </Routes>
     </>
   )
